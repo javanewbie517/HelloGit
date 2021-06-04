dbe96a6d5dca62b039e8d103e6d04d1cf02ddee6
angel <1929618793@qq.com>
2021-06-04 15:36:18 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/sxf/HelloGit.java
@@ -0,0 +1,11 @@
+package com.sxf;
+
+/**
+ * Describe
+ * Created by sxf on 2021/6/4$
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
